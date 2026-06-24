63a3a1a919f631e60b5f990960ed10f8462eaee2
yiyoZX <105678117+yiyoZX@users.noreply.github.com>
2026-06-24 12:25:31 -0400

    testeo intenso

--- Frontend/RAEC/src/pages/Dashboard.jsx
+++ Frontend/RAEC/src/pages/Dashboard.jsx
@@ -36,7 +36,7 @@ const Dashboard = () => {
       
       case 'profesor':
         return [
-          { label: 'Registrar actividad extracurricular', to: '/registrar', icon: registroIcon },
+          { label: 'Registrar actividad cosas que probar', to: '/registrar', icon: registroIcon },
           { label: 'Reportes de actividades', to: '/reportesAcademicos', icon: reportesIcon },
         ];
       
